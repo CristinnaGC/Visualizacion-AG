16db39a8af7f52a9e915ae2644cdc7cd054e5b7c
CristinnaGC <114524799+CristinnaGC@users.noreply.github.com>
2022-11-17 12:29:04 +0100

    3 files modified

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // Import stylesheets
 import './style.css';
 
-//Gráfico de columnas. Mi 1º gráfico
+
+//GRÁFICO DE COLUMNAS:
 var visualization = d3plus.viz()
     .container("#viz1")
+    //Datos incorporados directamente:
     .data([{"year": 1991, "name":"alpha", "value": 15},
            {"year": 1992, "name":"alpha", "value": 20},])
     .type("bar")
@@ -12,10 +14,8 @@ var visualization = d3plus.viz()
     .y("value")
     .draw()
 
-
-//ERROR
-d3.json
-("https://raw.githubusercontent.com/raul27868/07MBIG-Visualizacion-Actividades-Guiadas/master/columnas.json", function(data) {
+//Obteniendo los datos de la url indicada:
+d3.json("https://raw.githubusercontent.com/raul27868/07MBIG-Visualizacion-Actividades-Guiadas/master/columnas.json", function(data) {
     var visualization = d3plus.viz()
     .container("#viz2")
     .data(data)
@@ -25,4 +25,56 @@ d3.json
     .y('value')
     .axes({ ticks: 'false' })
     .draw();
-    });    
+    });
+
+//Datos de la url (por si fueran necesarios):
+//[
+//  {"year": 1991, "name":"alpha", "value": 15},
+//  {"year": 1991, "name":"beta", "value": 10},
+//  {"year": 1991, "name":"gamma", "value": 5},
+//  {"year": 1991, "name":"delta", "value": 50},
+//  {"year": 1992, "name":"alpha", "value": 20},
+//  {"year": 1992, "name":"beta", "value": 10},
+//  {"year": 1992, "name":"gamma", "value": 10},
+//  {"year": 1992, "name":"delta", "value": 43},
+//  {"year": 1993, "name":"alpha", "value": 30},
+//  {"year": 1993, "name":"beta", "value": 40},
+//  {"year": 1993, "name":"gamma", "value": 20},
+//  {"year": 1993, "name":"delta", "value": 17},
+//  {"year": 1994, "name":"alpha", "value": 60},
+//  {"year": 1994, "name":"beta", "value": 60},
+//  {"year": 1994, "name":"gamma", "value": 25},
+//  {"year": 1994, "name":"delta", "value": 32}
+//]
+
+
+//DIAGRAMA DE CAJAS Y BIGOTES:  
+//Datos:
+var datos = [
+    {"year": 1991, "name":"alpha", "value": 15},
+    {"year": 1992, "name":"alpha", "value": 34},
+    {"year": 1991, "name":"alpha2", "value": 17},
+    {"year": 1992, "name":"alpha2", "value": 65},
+    {"year": 1991, "name":"beta", "value": 10},
+    {"year": 1992, "name":"beta", "value": 10},
+    {"year": 1991, "name":"beta2", "value": 40},
+    {"year": 1992, "name":"beta2", "value": 38},
+    {"year": 1991, "name":"gamma", "value": 5},
+    {"year": 1992, "name":"gamma", "value": 10},
+    {"year": 1991, "name":"gamma2", "value": 20},
+    {"year": 1992, "name":"gamma2", "value": 34},
+    {"year": 1991, "name":"delta", "value": 50},
+    {"year": 1992, "name":"delta", "value": 43},
+    {"year": 1991, "name":"delta2", "value": 17},
+    {"year": 1992, "name":"delta2", "value": 35}
+  ]
+
+//Gráfico:
+var visualization = d3plus.viz()
+    .container("#viz3")
+    .data(datos)
+    .type("box")
+    .id("name")
+    .x("year")
+    .y("value")
+    .draw()      
